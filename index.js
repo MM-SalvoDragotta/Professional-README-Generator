e63c050ffdac6c623cd7d86f7f3a4d4db0934f7e
Salvo Dragotta <salvo.dragotta@mottmac.com>
2021-08-01 15:41:40 +1000

    Add validation to qeustions

--- index.js
+++ index.js
@@ -8,22 +8,54 @@ const questions = [
     {
         type: "input",
         message: "GitHub username",
-        name: "GitHub"
-    },
+        name: "GitHub",
+        validate: value =>{
+            if (value){
+                return true;  
+            }else{
+                console.log('Please provide the GitHub username to continue!');
+                return false;
+            }
+        }        
+    },   
     {
         type: "input",
         message: "Email address?",
-        name: "email"
+        name: "email",
+        validate: value =>{
+            if (value){
+                return true;  
+            }else{
+                console.log('Please provide the email to continue!');
+                return false;
+            }
+        }        
     },
     {
         type: "input",
         message: "Project's title?",
-        name: "title"
+        name: "title",
+        validate: value =>{
+            if (value){
+                return true;  
+            }else{
+                console.log(`Please provide the Project's title to continue!`);
+                return false;
+            }
+        }        
     },
     {
         type: "input",
         message: "Description of your project.",
-        name: "description"
+        name: "description",
+        validate: value =>{
+            if (value){
+                return true;  
+            }else{
+                console.log('Please provide the description to continue!');
+                return false;
+            }
+        }        
     },
     {
         type: "list",
@@ -37,28 +69,68 @@ const questions = [
             "BSD 3-Clause",
             "Mozilla Public License 2.0",
             "The Unlicense"
-        ]
+        ],
+        validate: value =>{
+            if (value){
+                return true;  
+            }else{
+                console.log('Please select the licence to continue!');
+                return false;
+            }
+        }        
     },
     {
         type: "input",
         message: "Usage of your project",
-        name: "usage"
+        name: "usage",
+        validate: value =>{
+            if (value){
+                return true;  
+            }else{
+                console.log('Please provide the usage to continue!');
+                return false;
+            }
+        }        
     },
     {
         type: "input",
         message: "Instructions regarding how to install your project",
         name: "install",
+        validate: value =>{
+            if (value){
+                return true;  
+            }else{
+                console.log('Please provide installation instruction to continue!');
+                return false;
+            }
+        }        
     },
     {
         type: "input",
         message: "Let the user know how to test your project",
         name: "tests",
-        default: "npm run test"
+        default: "npm run test",
+        validate: value =>{
+            if (value){
+                return true;  
+            }else{
+                console.log('Please provide testing instruction to continue!');
+                return false;
+            }
+        }        
     },   
     {
         type: "input",
         message: "Let the user know about contributing to this project",
-        name: "contribute"
+        name: "contribute",
+        validate: value =>{
+            if (value){
+                return true;  
+            }else{
+                console.log('Please provide instruction on how to contribute to continue!');
+                return false;
+            }
+        }        
     },
 
 ];
@@ -69,7 +141,7 @@ function writeToFile(fileName, data) {}
 // TODO: Create a function to initialize app
 function init() {
     console.log(markdown.renderLicenseBadge("MIT"));
-    
+
 
 }
 
