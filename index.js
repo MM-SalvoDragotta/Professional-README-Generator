1c3301083e859ae84128b24d5d505a953f57b8eb
Salvo Dragotta <salvo.dragotta@mottmac.com>
2021-08-01 15:53:37 +1000

    Add generateMarkdown function

--- index.js
+++ index.js
@@ -108,7 +108,7 @@ const questions = [
     {
         type: "input",
         message: "Let the user know how to test your project",
-        name: "tests",
+        name: "test",
         default: "npm run test",
         validate: value =>{
             if (value){
@@ -136,11 +136,19 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if (err)
+            throw err;
+        console.log(' README file created!')
+    });
+};
+
 
 // TODO: Create a function to initialize app
 function init() {
-    console.log(markdown.renderLicenseBadge("MIT"));
+    //console.log(markdown.renderLicenseBadge("MIT"));
+
 
 
 }
